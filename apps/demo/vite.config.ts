aa841ac62db338027592b7f8d54a6e296d0cd1f4
Tamila Kenessova <tamilakenessova@gmail.com>
2026-04-30 01:55:57 +0200

    Prepare library packages for release

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    commonjsOptions: {
+      include: [
+        /node_modules/,
+        /packages\/core/,
+        /packages\/react-renderer/,
+        /packages\/server-renderer/
+      ]
+    }
+  }
 });
